d820c111a2bb00744c283994de0be6e1cbd52fbc
vinny-boob <makarivincent744@gmail.com>
2025-11-20 12:16:09 -0800

    first commit

--- ProjectOne/Myproject/Myproject/settings.py
+++ ProjectOne/Myproject/Myproject/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.Path.join(BASE_DIR,'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL='/staric/'
+STATICFILE_DIRS
